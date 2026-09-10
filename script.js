0cfd5dd18e151b35462e0d265a9d4d4753f5803a
beatryz1284 <beatryzchagasdossantos1@gmail.com>
2026-09-10 09:07:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+function continuar() {
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+
+    if (nome === "" || email === "" || senha === "") {
+        alert("Erro! Talvez esteja tudo certo, mas alguns campos parecem estar vazios.");
+        return;
+    }
+
+    alert("Cadastro aparentemente concluído com sucesso... ou não.");
+    window.location.href = "final.html";
+}
